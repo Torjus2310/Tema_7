025826aeb17bd7b3c66fa850e68b42342b929142
Torjus2310 <56019539+Torjus2310@users.noreply.github.com>
2020-04-17 11:24:25 +0200

    fiks kanskje

--- oppgavesettene/Oppgavesett_04/o2/index.js
+++ oppgavesettene/Oppgavesett_04/o2/index.js
@@ -3,13 +3,20 @@ let alder = document.querySelector ("#alder");
 let btn = document.querySelector ("#btn");
 let listVenn = document.querySelector ("#listVenn");
 
-/*let venner = [
+let venner = [
     { navn: "Snorre", alder: 28 },
     { navn: "Lise", alder: 24 },
     { navn: "Børre", alder: 32 }
-    ]*/
+    ]
 
-    
+    funtion visVenner() {
+        listVenn.innerHTML = "<div>Navn</div><div>Alder</div>";
+
+        for(const venn of venner) {
+            listVenn.innerHTML +=
+            <div class
+        }
+    }
     
 function LeggTilNavn() {
 
